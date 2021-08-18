8f16fa900590230f51b4762b0f31cc0e21e5f5dd
Ambertide <egeemirozkan24@gmail.com>
2021-08-18 11:36:51 +0300

    Specified origins.

--- jfteams_proxy/__init__.py
+++ jfteams_proxy/__init__.py
@@ -16,7 +16,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def create_app() -> FastAPI:
     app = FastAPI()
     app.add_middleware(CORSMiddleware,
-                       allow_origins=["*"],
+                       allow_origins=["https://localhost:3000", "http://localhost"],
                        allow_credentials=True,
                        allow_methods=["*"],
                        allow_headers=["*"])
